217105ded0d32b1721b56be05478e74b98e0a0e1
taeaeaexin <kjk06119@gmail.com>
2025-03-26 15:33:09 +0900

    250326

---
+++ Baekjoon/src/S5_11723_집합.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S5_11723_집합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+
+    }
+}
