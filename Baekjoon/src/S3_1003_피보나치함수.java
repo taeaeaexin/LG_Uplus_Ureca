981a1b4085587843012c5692102434b107b79efd
taeaeaexin <kjk06119@gmail.com>
2025-03-31 17:41:06 +0900

    250331 1741

--- Baekjoon/src/S3_1003_피보나치함수.java
+++ Baekjoon/src/S3_1003_피보나치함수.java
@@ -1,35 +1,35 @@
-import java.io.*;
-
-public class S3_1003_피보나치함수 {
-    public static int zero;
-    public static int one;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
-            zero = 0;
-            one = 0;
-            fibonacci(N);
-            bw.write(zero + " " + one + "\n");
-        }
-        bw.flush();
-        br.close();
-        bw.close();
-    }
-
-    public static int fibonacci(int n) {
-        if (n == 0) {
-            zero++;
-            return 0;
-        } else if (n == 1) {
-            one++;
-            return 1;
-        } else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
-        }
-    }
-}
+//import java.io.*;
+//
+//public class S3_1003_피보나치함수 {
+//    public static int zero;
+//    public static int one;
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        int T = Integer.parseInt(br.readLine());
+//        for (int i = 0; i < T; i++) {
+//            int N = Integer.parseInt(br.readLine());
+//            zero = 0;
+//            one = 0;
+//            fibonacci(N);
+//            bw.write(zero + " " + one + "\n");
+//        }
+//        bw.flush();
+//        br.close();
+//        bw.close();
+//    }
+//
+//    public static int fibonacci(int n) {
+//        if (n == 0) {
+//            zero++;
+//            return 0;
+//        } else if (n == 1) {
+//            one++;
+//            return 1;
+//        } else {
+//            return fibonacci(n - 1) + fibonacci(n - 2);
+//        }
+//    }
+//}
