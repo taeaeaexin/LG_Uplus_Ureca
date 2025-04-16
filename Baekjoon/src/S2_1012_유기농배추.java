ae19a7bf21eabc24310d9e5db98b851cb4ede8a7
taeaeaexin <re_fuse@naver.com>
2025-04-16 15:03:36 +0900

    250416 1503

--- Baekjoon/src/S2_1012_유기농배추.java
+++ Baekjoon/src/S2_1012_유기농배추.java
@@ -4,13 +4,59 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class S2_1012_유기농배추 {
+    public static int M, N, K;
+    public static int[][] arr;
+    public static boolean[][] isVisit;
+
+    public static int[] dy = {1, 0, -1, 0};
+    public static int[] dx = {0, 1, 0, -1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
 
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            arr = new int[N][M];
+            isVisit = new boolean[N][M];
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                arr[y][x] = 1;
+            }
+
+            int result = 0;
+            for (int y = 0; y < N; y++) {
+                for (int x = 0; x < M; x++) {
+                    if (arr[y][x] == 1 && !isVisit[y][x]) {
+                        dfs(y, x);
+                        result++;
+                    }
+                }
+            }
+
+            System.out.println(result);
+        }
+    }
+
+    public static void dfs(int y, int x) {
+        isVisit[y][x] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny >= 0 && ny < N && nx >= 0 && nx < M) {
+                if (arr[ny][nx] == 1 && !isVisit[ny][nx]) {
+                    dfs(ny, nx);
+                }
+            }
+        }
     }
 }
