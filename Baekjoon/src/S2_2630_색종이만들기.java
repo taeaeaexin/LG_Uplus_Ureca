1b28e8169d792871e8537d0a91650da8326bb575
taeaeaexin <re_fuse@naver.com>
2025-04-18 14:59:30 +0900

    250418 1459

--- Baekjoon/src/S2_2630_색종이만들기.java
+++ Baekjoon/src/S2_2630_색종이만들기.java
@@ -4,15 +4,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class S2_2630_색종이만들기 {
-    public static int N;
-    public static int white;
-    public static int black;
+    public static int[] color = {0,0};
     public static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         arr = new int[N][N];
         for (int i = 0; i < N; i++) {
             st  = new StringTokenizer(br.readLine()," ");
@@ -21,34 +19,37 @@ public class S2_2630_색종이만들기 {
             }
         }
 
-        white = 0;
-        black = 0;
         cutPaper(0,0, N);
-        System.out.println(white);
-        System.out.println(black);
+        System.out.println(color[0]);
+        System.out.println(color[1]);
     }
 
     public static void cutPaper(int y, int x, int n){
-        int color = arr[y][x];
+        if(n == 1) {
+            color[arr[y][x]]++;
+            return;
+        }
+
+        int c = arr[y][x];
         boolean flag = true;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(arr[i][j] != color){
+        for (int i = y; i < y+n; i++) {
+            for (int j = x; j < x+n; j++) {
+                if(arr[i][j] != c){
                     flag = false;
                     break;
                 }
             }
         }
 
-        if(flag){
-            if(color == 0) white++;
-            else black++;
+        if(flag) {
+            color[c]++;
+            return;
         }
 
-        cutPaper(0,0,n/2);
-        cutPaper(0,n/2, n/2);
-        cutPaper(n/2,0,n/2);
-        cutPaper(n/2,n/2,0);
+        cutPaper(y,x,n/2);
+        cutPaper(y,x+n/2,n/2);
+        cutPaper(y+n/2,x,n/2);
+        cutPaper(y+n/2,x+n/2,n/2);
     }
 }
