6ea01fce5d07faa4acefd999f4b9d650fdc04b9c
taeaeaexin <re_fuse@naver.com>
2025-05-06 12:37:28 +0900

    250506

--- Baekjoon/src/G5_1931_회의실배정.java
+++ Baekjoon/src/G5_1931_회의실배정.java
@@ -1,18 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class G5_1931_회의실배정 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int[] arr = new int[N];
+        StringTokenizer st;
+        int[][] arr = new int[N][2];
 
         for (int i = 0; i < N; i++) {
-            arr[0] = Integer.parseInt(st.nextToken());
-            arr[1] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine()," ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(arr, (a, b) -> {
+            if(a[1] == b[1]) return a[0] - b[0];
+            return a[1] - b[1];
+        });
+
+        int result = 0;
+        int end = 0;
+
+        for (int i = 0; i < N; i++) {
+            if(end <= arr[i][0]){
+                end = arr[i][1];
+                result++;
+            }
+        }
+
+        System.out.println(result);
     }
 }
